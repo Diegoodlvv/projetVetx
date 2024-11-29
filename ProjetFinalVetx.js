00435812357dc1017009efc060ec22c74263571b
Diegoodlvv <deoliveiradiego953@gmail.com>
2024-11-30 00:52:50 +0100

    burgermenu

---
+++ ProjetFinalVetx.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector('.burgermenu_h')
+var btn2 = document.querySelector('.BurgerMenu')
+
+
+btn.addEventListener('click' , () => {
+    btn2.classList.toggle('BurgerMenu');
+});
+
