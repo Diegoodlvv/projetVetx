2e18cf2dc01f503c141720ecc5b7a0a4ce0a35c4
Diegoodlvv <deoliveiradiego953@gmail.com>
2024-12-05 18:54:46 +0100

    carrousel

--- ProjetFinalVetx.js
+++ ProjetFinalVetx.js
@@ -4,3 +4,40 @@ const text = document.querySelector('.BurgerButtons')
 btn.addEventListener('click', () => {
     text.classList.toggle('BurgerButtons2');
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const carouselWrapperBloc1 = document.getElementById('carouselWrapperBloc1');
+    const backBloc1 = document.getElementById('backBloc1');
+    const nextBloc1 = document.getElementById('nextBloc1');
+
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        const translateValue = -currentIndex * 774;
+        carouselWrapperBloc1.style.transform = translateX(${ translateValue }px);
+    }
+
+    function nextSlide() {
+        if (currentIndex < 2) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        updateCarousel();
+    }
+
+    function prevSlide() {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = 2;
+        }
+        updateCarousel();
+    }
+    //CAROUSSEL NUMER0 2
+    backBloc1.addEventListener('click', nextSlide);
+    nextBloc1.addEventListener('click', prevSlide);
+
+});
